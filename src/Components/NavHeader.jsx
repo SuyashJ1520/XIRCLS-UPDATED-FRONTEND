edbddf1da89c4f18c65c48d2ef017f386f1da141
SuYash0804 <112413154+SuYash0804@users.noreply.github.com>
2023-04-11 13:21:30 +0530

    added header and few more changes

--- src/Components/NavHeader.jsx
+++ src/Components/NavHeader.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { ArrowLeft } from 'react-feather'
+// import { useNavigate } from 'react-router-dom'
+import { Col, Container, Row } from 'reactstrap'
 
 const NavHeader = () => {
+
+  // const navigateTo = useNavigate()
   return (
-    <div>NavHeader</div>
+    <Container fluid className='p-3'>
+      <Row>
+        <Col className='d-flex justify-content-between align-items-center'>
+          <div className='d-flex gap-2 align-items-center'>
+            <ArrowLeft className='cursor-pointer' />
+            <h2 className='m-0' style={{fontWeight: 600}}>Header Title</h2>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
