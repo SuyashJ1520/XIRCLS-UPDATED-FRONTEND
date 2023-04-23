0c8a04e3a01040b24cfdff598208f4c1c64373cd
SuyashJ1520 <121536098+SuyashJ1520@users.noreply.github.com>
2023-04-23 21:09:26 +0530

    some minor changes to navbar

--- src/Components/NavWrapper/Sidebar.jsx
+++ src/Components/NavWrapper/Sidebar.jsx
@@ -12,25 +12,23 @@ const Sidebar = ({ navCheck, setNavCheck, setNavHover, navHover, navWidth }) => 
       onMouseLeave={() => {
         setNavHover(false)
       }}>
-      <div className={`d-flex justify-content-between align-items-center p-3`}>
+      <div className={`d-flex justify-content-between align-items-center py-3`}>
         <div className='text-center' style={{ width: !(navHover || navCheck) ? '100%' : `${100 / 3}%`, overflow: 'hidden' }}>
           {!(navHover || navCheck) && <img src={startLogo} alt="logo" height={35} />}</div>
         <div className='text-center' style={{ width: !(navHover || navCheck) ? '0px' : `${100 / 1}%`, overflow: 'hidden' }}>
           <img className='transition-smooth' src={centerLogo} alt="logo" style={{ scale: (navHover || navCheck) ? '1' : '0.25' }} height={35} />
         </div>
-        <div style={{ width: !(navHover || navCheck) ? '0px' : `${100 / 3}%`, overflow: 'hidden' }} className={`d-flex justify-content-end align-self-start transition-smooth`}>
-          {(navHover || navCheck) &&
-            <>
-              <input onChange={(e) => {
-                setNavCheck(e.target.checked)
-              }} type='checkbox' id='nav-check' className='d-none' />
-              <label htmlFor='nav-check' className='d-flex justify-content-center align-items-center position-relative' style={{ cursor: 'pointer' }}>
-                <Circle size={20} color='white' />
-                <Circle size={navCheck ? 12 : 0} className='position-absolute transition-smooth' fill={navCheck ? 'white' : 'black'} />
-              </label>
-            </>
-          }
-        </div>
+        {(navHover || navCheck) &&
+          <div style={{ width: !(navHover || navCheck) ? '0px' : `${100 / 3}%`, overflow: 'hidden' }} className={`d-flex justify-content-end align-self-start transition-smooth px-3`}>
+            <input onChange={(e) => {
+              setNavCheck(e.target.checked)
+            }} type='checkbox' id='nav-check' className='d-none' />
+            <label htmlFor='nav-check' className='d-flex justify-content-center align-items-center position-relative' style={{ cursor: 'pointer' }}>
+              <Circle size={20} color='white' />
+              <Circle size={navCheck ? 12 : 0} className='position-absolute transition-smooth' fill={navCheck ? 'white' : 'black'} />
+            </label>
+          </div>
+        }
       </div>
     </div>
   )
